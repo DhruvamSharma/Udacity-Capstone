80b7b3ea30534b39b9b836a0de438d968e7130a1
Dhruvam <dhruvamssharma@gmail.com>
2018-11-05 22:43:50 +0530

    started setting up project

--- AndroidProject/app/src/main/java/com/udafil/dhruvamsharma/udacity_capstone/MainActivity.java
+++ AndroidProject/app/src/main/java/com/udafil/dhruvamsharma/udacity_capstone/MainActivity.java
@@ -3,11 +3,16 @@ package com.udafil.dhruvamsharma.udacity_capstone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.MobileAds;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        MobileAds.initialize(this, getResources().getString(R.string.app_id));
     }
 }
