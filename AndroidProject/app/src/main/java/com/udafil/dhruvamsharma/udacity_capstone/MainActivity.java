71c0d9d17c7ed6791b989575acc8d15ffbf6c26a
Dhruvam <dhruvamssharma@gmail.com>
2018-11-07 23:22:27 +0530

    Task 1: Migrated to androidx. Added Material Component Dependency

--- AndroidProject/app/src/main/java/com/udafil/dhruvamsharma/udacity_capstone/MainActivity.java
+++ AndroidProject/app/src/main/java/com/udafil/dhruvamsharma/udacity_capstone/MainActivity.java
@@ -1,6 +1,6 @@
 package com.udafil.dhruvamsharma.udacity_capstone;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.ads.MobileAds;
