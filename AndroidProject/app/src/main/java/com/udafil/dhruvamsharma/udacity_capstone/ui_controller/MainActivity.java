49b75ebc4a3ea25f0a4dbde5504c148a6ee44501
Dhruvam <dhruvamssharma@gmail.com>
2018-11-08 13:05:43 +0530

    Task 2: Part 2: BottomSheet Created

--- AndroidProject/app/src/main/java/com/udafil/dhruvamsharma/udacity_capstone/ui_controller/MainActivity.java
+++ AndroidProject/app/src/main/java/com/udafil/dhruvamsharma/udacity_capstone/ui_controller/MainActivity.java
@@ -1,13 +1,17 @@
 package com.udafil.dhruvamsharma.udacity_capstone.ui_controller;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.udafil.dhruvamsharma.udacity_capstone.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     //recycler view for all the tasks
     private RecyclerView mTaskList;
+    private LinearLayout mBottomSheet;
+    private BottomSheetBehavior mBottomSheetBehavior;
 
     /**
      * Method when the activity is created
@@ -76,6 +82,42 @@ public class MainActivity extends AppCompatActivity {
         data.add("tata");
 
         return data;
+
+    }
+
+    private void setupBottomSheet() {
+
+        mBottomSheet = findViewById(R.id.activity_main_bottom_sheet_bs);
+
+        mBottomSheetBehavior = BottomSheetBehavior.from(mBottomSheet);
+
+        mBottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View view, int i) {
+
+                switch ( i ) {
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED: {
+
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_COLLAPSED: {
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View view, float v) {
+
+            }
+        });
+
 
     }
 }
